56664c4c709af659980e5f7baba321a2f22825ba
TheoSann <theoleroy478@gmail.com>
2023-02-25 06:03:00 -0800

    Numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - checks for a digit (0 through 9)
+ *
+ * Return: Always 0
+ */
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+	_putchar(a + '0');
+	}
+	_putchar("\n");
+}
